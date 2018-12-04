e7035d52805d0e5fcd16e1428ef85eff769983d3
Jesús Macias Portela <jesus.maciasportela@telefonica.com>
2018-12-05 00:18:32 +0100

    Add some modifications to read hash list from file

--- README.md
+++ README.md
@@ -5,25 +5,12 @@ This tools can crack md5 hash over http://hashkiller.co.uk
 
 Installation
 ============
-1. Install OCR:
-
-    For linux user:
-    `sudo apt-get install python-imaging tesseract-ocr`
-
-    For Mac user:
-    `brew install tesseract`
-
-2. Install requirements
+1. Install requirements
 
     `pip install -r requirements.txt`
 
-
 ###Command
 
-You can use this tool with this command `./md5crack [hash]`
+You can use this tool with this command `./md5crack FILE`
+example: `./md5crack.py hashes.txt`
 
-example: `./md5crack.py 4dd39f49f898c062283963c187532af8`
-
-If you want to input captcha manually, you can pass `-m` argument
-
-example: `./md5crack.py -m 4dd39f49f898c062283963c187532af8`
--- md5crack.py
+++ md5crack.py
@@ -10,15 +10,15 @@ import os
 import requests
 import StringIO
 
-from ocr import image_to_string
 from PIL import Image
 from pyquery import PyQuery
 
 
 HOST = 'https://www.hashkiller.co.uk'
+SLICESIZE = 64
+FILEOUT = "results.txt"
 
-
-def crack(md5, auto=True):
+def crack(md5List, auto=True):
     scraper = cfscrape.create_scraper()
     response = scraper.get(HOST + '/md5-decrypter.aspx')
 
@@ -33,31 +33,13 @@ def crack(md5, auto=True):
 
     # Trying to decaptcha image
     captcha_image = Image.open(StringIO.StringIO(image_content))
-
-    if auto:
-        img = captcha_image.load()
-        pix = captcha_image.size
-
-        for x in xrange(pix[0]):
-            for y in xrange(pix[1]):
-                if img[x, y][0] < 107 or img[x, y][1] < 4:
-                    img[x, y] = (0, 0, 0, 255)
-                if img[x, y][2] > 0:
-                    img[x, y] = (255, 255, 255, 0)
-
-        captcha = image_to_string(captcha_image)
-        captcha = filter(str.isalnum, captcha).upper()
-    else:
-        captcha_image.show()
-        captcha = raw_input("[+] Input captcha: ")
-
-    if len(captcha) != 6:
-        return False
+    captcha_image.show()
+    captcha = raw_input("[+] Input captcha: ")
 
     scraper = cfscrape.create_scraper(sess=scraper)
     response = scraper.post(HOST + '/md5-decrypter.aspx', data={
         'ctl00$ScriptMan1': 'ctl00$content1$updDecrypt|ctl00$content1$btnSubmit',
-        'ctl00$content1$txtInput': md5,
+        'ctl00$content1$txtInput': md5List,
         'ctl00$content1$txtCaptcha': captcha,
         '__EVENTTARGET': '',
         '__EVENTARGUMENT': '',
@@ -69,44 +51,48 @@ def crack(md5, auto=True):
     })
     response = PyQuery(response.content)
     status = response('#content1_lblStatus').text()
-    result = response('#content1_lblResults .text-green').text()
-
+    result = response('#content1_lblResults .text-green, #content1_lblResults .text-red').text().replace('[Not found]', 'Not_found').split()
     return status, result
 
+def chunks(listMD5, chunkSize):
+    chunkSize = max(1, chunkSize)
+    return (listMD5[i:i+chunkSize] for i in xrange(0, len(listMD5), chunkSize))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Crack md5hash over https://hashkiller.co.uk')
-    parser.add_argument('md5', nargs='+', help='md5 hash')
-    parser.add_argument('-m', '--manual-captcha', default=False,
-                        help='Manual captcha image. (GUI only)',
-                        action='store_true')
+    parser.add_argument('file', nargs='+', help='file with md5 hash list')
     args = parser.parse_args()
-    if args.md5:
-        md5hash = args.md5[0]
-        if len(md5hash) != 32:
-            print "[!] Your hash is not md5!"
-            exit()
+    if args.file:
+        inputFile = args.file[0]
         if os.name == "posix":
             os.system("clear")
         else:
             os.system("cls")
-        print "[+] Trying to crack your hash..."
+        print "[+] Looking for your hashes..."
 
         while True:
             try:
-                auto = False if args.manual_captcha else True
-                cracking = crack(md5hash, auto=auto)
-                if cracking:
-                    if 'CAPTCHA' in cracking[0]:
-                        continue
-                    elif 'Failed' in cracking[0]:
-                        print "[-] Result: Hash not found!"
-                    else:
-                        print "[+] Result: %s" % cracking[1]
-                    exit()
-                else:
-                    continue
+                with open(inputFile, 'r') as f:
+                    listMD5 = f.readlines()
+                # Max 64 hashes per request
+                partials = chunks(listMD5, SLICESIZE)
+                with open(FILEOUT, 'w') as outf:
+                    for chunk in partials:
+                        cracking = crack(''.join(chunk))
+                        if cracking:
+                            print cracking[1]
+                            print cracking[0]
+                            for result in cracking[1]:
+                                if result != 'Not_found':
+                                    outf.write( result + '\n')
+                                else:
+                                    outf.write('\n')
+                            print "[+] Chunk Results: " + cracking[0]
+                        else:
+                            continue
+                print: "Check file " + FILEOUT
+                exit()
             except KeyboardInterrupt:
                 print "\b\b[!] Thanks for using this tool."
                 exit()
